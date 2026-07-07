e2792509b9320eb90c976e620ed31c663386e229
miliibarra73-lang <mili.ibarra73@hotmail.com>
2026-07-07 13:01:19 -0300

    Update Escenario_4.py
    Se agrega la función de inicio de sesión con manejo de intentos

--- Escenario_4.py
+++ Escenario_4.py
@@ -17,3 +17,26 @@ usuarios = {
 
 usuario_autenticado = None  
 intentos_restantes = 3
+# Sección de autenticación
+
+def iniciar_sesion():
+    global usuario_autenticado, intentos_restantes
+
+    print("\n--- BIENVENIDO AL BANCO DIGITAL ---")
+
+    while intentos_restantes > 0:
+        dni = input("Ingrese su DNI (Usuario): ").strip()
+        pin = input("Ingrese su clave PIN de 4 dígitos: ").strip()
+
+        if dni in usuarios and usuarios[dni]["pin"] == pin:
+            usuario_autenticado = dni
+            print(f"\n¡Ingreso exitoso! Bienvenido/a {usuarios[dni]['nombre']}.")
+            return True
+        else:
+            intentos_restantes -= 1
+            print(f"[ERROR] Usuario o PIN incorrectos.")
+            print(f"Intentos restantes: {intentos_restantes}\n")
+
+    print("\n[ALERTA] Tarjeta retenida por seguridad. Comuniqúese con el banco.")
+    return False
+    
